718d87afa7d7a548340fa4b8940d069f02bf8a88
SaToGo <stoledano97@gmail.com>
2023-03-25 16:48:54 +0100

    Add input, button and Tasks Title

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import React, { Component } from 'react'
 //import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      taskArray: []
+    }
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <input type="text"></input>
+        <button>Add Task</button>
+        <h2>Tasks</h2>
+      </div>
+    );
+  }
 }
 
 export default App;
